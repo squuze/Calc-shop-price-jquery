165204577b32bff1579da52c58330e75ae0b0867
squuze <nikolaskohno@gmail.com>
2019-01-23 00:33:38 +0500

    logic calc product -> price(jquery)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
   $('.calc select').change(function () {
     var edition = $('select#cart_edition').val();
-    console.log(edition);
+    var paper = $('select#card_paper').val();
+    var color = $('select#card_color').val();
+    var paper_ratio = $('select#card_paper option:selected').attr('data-paper-ratio');
+    var print_file = $('select#card_color option:selected').attr('data-print-file');
+
+    var division = 30;
+    var final_edition = edition / division;
+    var print = $('select#card_color option:selected').attr('data-print');
+
+    var price = final_edition * paper_ratio + parseInt(print_file) + final_edition*print;
+    $('span#final_price').text(price);
   })
 });
